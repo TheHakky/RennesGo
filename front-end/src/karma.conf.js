51e846a005700a2ea3ecf058a570ffdb69434b83
Emre Aydinli <34942139+TheHakky@users.noreply.github.com>
2019-11-20 15:30:10 +0100

    Update karma.conf.js

--- front-end/src/karma.conf.js
+++ front-end/src/karma.conf.js
@@ -49,7 +49,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'MyChrome'],
     singleRun: true,
     restartOnFileChange: true,
 
