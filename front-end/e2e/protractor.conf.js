ae4a5f64368959c17ad0d8a9db6e9c02c7f364f1
Emre Aydinli <34942139+TheHakky@users.noreply.github.com>
2019-11-29 22:50:43 +0100

    Update protractor.conf.js

--- front-end/e2e/protractor.conf.js
+++ front-end/e2e/protractor.conf.js
@@ -8,7 +8,7 @@ exports.config = {
   specs: [
     './src/**/*.e2e-spec.ts'
   ],
-  chromeDriver: './node_modules/protractor/node_modules/webdriver-manager/selenium/chromedriver_78.0.3904.105',
+  chromeDriver: path.join(paths.e2e, './node_modules/protractor/node_modules/webdriver-manager/selenium/chromedriver_2.37.544315'),
   capabilities: {
     'browserName': 'chrome',
     chromeOptions: {
